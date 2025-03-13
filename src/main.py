568f163016a2c6311cf3d6075f599787c6ca61c2
riddler_rid <unkownrid@gmail.com>
2025-03-13 12:53:19 +0300

    add user routers

--- src/main.py
+++ src/main.py
@@ -1,8 +1,42 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from fastapi_users import FastAPIUsers
+
+from src.auth.backend import auth_backend
+from src.auth.schemas import UserRead, UserCreate, UserUpdate
+from src.auth.user_manager import get_user_manager
+from src.database import User
 
 app = FastAPI()
 
 
-@app.get("")
-async def root():
-    return {"message": "Hello World"}
+fastapi_users = FastAPIUsers[User, int](
+    get_user_manager,
+    [auth_backend],
+)
+
+app.include_router(
+    fastapi_users.get_auth_router(auth_backend),
+    prefix="/auth/jwt",
+    tags=["auth"],
+)
+
+
+app.include_router(
+    fastapi_users.get_register_router(UserRead, UserCreate),
+    prefix="/auth",
+    tags=["auth"],
+)
+
+
+app.include_router(
+    fastapi_users.get_users_router(UserRead, UserUpdate),
+    prefix="/users",
+    tags=["users"],
+)
+
+
+current_user = fastapi_users.current_user()
+
+@app.get("/protected-route")
+def protected_route(user: User = Depends(current_user)):
+    return f"Hello, {user.email}"
