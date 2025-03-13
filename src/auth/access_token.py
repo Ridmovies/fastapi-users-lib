4a2696d159718b5988769f1087def95ee9dd2db8
riddler_rid <unkownrid@gmail.com>
2025-03-13 11:43:01 +0300

    add access token

---
+++ src/auth/access_token.py
@@ -0,0 +1,20 @@
+from fastapi import Depends
+from fastapi_users_db_sqlalchemy.access_token import SQLAlchemyBaseAccessTokenTable, SQLAlchemyAccessTokenDatabase
+from sqlalchemy import Integer, ForeignKey
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import declared_attr, Mapped, mapped_column
+
+from src.database import Base, get_async_session
+
+
+class AccessToken(SQLAlchemyBaseAccessTokenTable[int], Base):
+    @declared_attr
+    def user_id(cls) -> Mapped[int]:
+        return mapped_column(Integer, ForeignKey("user.id", ondelete="cascade"), nullable=False)
+
+
+
+async def get_access_token_db(
+    session: AsyncSession = Depends(get_async_session),
+):
+    yield SQLAlchemyAccessTokenDatabase(session, AccessToken)
